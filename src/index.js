4d875349bb0d1400788c4411102492135ba116e6
Maboela Maserame <maboelamaserame@gmail.com>
2023-05-12 18:23:03 +0200

    Add to do list structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,83 @@
-// eslint-disable-next-line
-import _ from 'lodash';
 import './style.css';
+import MyImage from './images/dots.png';
+import EnterIcon from './images/enter.png';
 
-function component() {
-  const element = document.createElement('div');
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
+let tasks = [
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 1,
+    icon: MyImage,
+  },
+  {
+    description: 'Task 2',
+    completed: true,
+    index: 2,
+    icon: MyImage,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 3,
+    icon: MyImage,
+  },
+];
+
+function clearCompletedTasks() {
+  tasks.forEach((task) => {
+    if (task.completed) {
+      const taskItem = document.querySelector(`[data-index="${task.index}"]`);
+      taskItem.parentNode.removeChild(taskItem);
+    }
+  });
+
+  tasks = tasks.filter((task) => !task.completed);
 }
-document.body.appendChild(component());
+
+function renderTasks() {
+  const taskList = document.getElementById('task-list');
+
+  const addNewPlaceholder = document.createElement('li');
+  addNewPlaceholder.className = 'task-item add-new';
+  addNewPlaceholder.innerHTML = `
+    <span class="task-description">Add new item</span>
+    <img src="${EnterIcon}" class="enter-icon">
+  `;
+  taskList.prepend(addNewPlaceholder);
+
+  tasks.forEach((task) => {
+    const taskItem = document.createElement('li');
+    taskItem.className = 'task-item';
+    taskItem.dataset.index = task.index;
+
+    const taskContent = document.createElement('div');
+    taskContent.className = 'task-content';
+    taskItem.appendChild(taskContent);
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    checkbox.className = 'task-checkbox';
+    taskContent.appendChild(checkbox);
+
+    const description = document.createElement('span');
+    description.textContent = task.description;
+    description.className = 'task-description';
+    taskContent.appendChild(description);
+
+    const icon = document.createElement('img');
+    icon.src = `${task.icon}`;
+    icon.className = 'task-icon';
+    taskItem.appendChild(icon);
+
+    taskList.appendChild(taskItem);
+  });
+
+  const clearButton = document.createElement('button');
+  clearButton.id = 'clear-button';
+  clearButton.textContent = 'Clear All Completed';
+  clearButton.addEventListener('click', clearCompletedTasks);
+  taskList.appendChild(clearButton);
+}
+
+renderTasks();
