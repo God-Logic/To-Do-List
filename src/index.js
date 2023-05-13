a77a91380d5a4b0ca4b1be2463e7624375c4dae2
Maboela Maserame <maboelamaserame@gmail.com>
2023-05-13 10:04:25 +0200

    Add add list functionality and remove list.

--- src/index.js
+++ src/index.js
@@ -1,71 +1,99 @@
-import './style.css';
-import MyImage from './images/dots.png';
-import EnterIcon from './images/enter.png';
+import "./style.css";
+import MyImage from "./images/dots.png";
+import EnterIcon from "./images/enter.png";
 
-const tasks = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 1,
-    icon: MyImage,
-  },
-  {
-    description: 'Task 2',
-    completed: true,
-    index: 2,
-    icon: MyImage,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-    icon: MyImage,
-  },
-];
+let tasks = [];
+
+function toggleTaskCompleted(event) {
+  const taskIndex = event.target.closest(".task-item").dataset.index;
+  tasks[taskIndex].completed = !tasks[taskIndex].completed;
+}
 
 function renderTasks() {
-  const taskList = document.getElementById('task-list');
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = "";
 
-  const addNewPlaceholder = document.createElement('li');
-  addNewPlaceholder.className = 'task-item add-new';
+  const addNewPlaceholder = document.createElement("li");
+  addNewPlaceholder.className = "task-item add-new";
   addNewPlaceholder.innerHTML = `
-    <span class="task-description">Add new item</span>
-    <img src="${EnterIcon}" class="enter-icon">
+    <form id="new-task-form">
+      <input type="text" class="new-task-input" placeholder="Add new item">
+      <img src="${EnterIcon}" class="enter-icon" alt="Enter icon">
+    </form>
   `;
   taskList.prepend(addNewPlaceholder);
 
-  tasks.forEach((task) => {
-    const taskItem = document.createElement('li');
-    taskItem.className = 'task-item';
-    taskItem.dataset.index = task.index;
+  tasks.forEach((task, index) => {
+    const taskItem = document.createElement("li");
+    taskItem.className = "task-item";
+    taskItem.dataset.index = index;
 
-    const taskContent = document.createElement('div');
-    taskContent.className = 'task-content';
+    const taskContent = document.createElement("div");
+    taskContent.className = "task-content";
     taskItem.appendChild(taskContent);
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
     checkbox.checked = task.completed;
-    checkbox.className = 'task-checkbox';
+    checkbox.className = "task-checkbox";
+    checkbox.addEventListener("change", toggleTaskCompleted);
     taskContent.appendChild(checkbox);
 
-    const description = document.createElement('span');
+    const description = document.createElement("span");
     description.textContent = task.description;
-    description.className = 'task-description';
+    description.className = "task-description";
     taskContent.appendChild(description);
 
-    const icon = document.createElement('img');
+    const icon = document.createElement("img");
     icon.src = `${task.icon}`;
-    icon.className = 'task-icon';
+    icon.className = "task-icon";
     taskItem.appendChild(icon);
 
     taskList.appendChild(taskItem);
   });
 
-  const clearButton = document.createElement('button');
-  clearButton.id = 'clear-button';
-  clearButton.textContent = 'Clear All Completed';
+  const clearButton = document.createElement("button");
+  clearButton.id = "clear-button";
+  clearButton.textContent = "Clear All Completed";
+  clearButton.addEventListener("click", clearCompletedTasks);
   taskList.appendChild(clearButton);
+
+  const newTaskForm = document.getElementById("new-task-form");
+  const newTaskInput = document.querySelector(".new-task-input");
+  const enterIcon = document.querySelector(".enter-icon");
+
+  newTaskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const taskDescription = newTaskInput.value.trim();
+    if (taskDescription) {
+      addNewTask(taskDescription);
+      newTaskInput.value = "";
+    }
+  });
+
+  enterIcon.addEventListener("click", (event) => {
+    event.preventDefault();
+    const taskDescription = newTaskInput.value.trim();
+    if (taskDescription) {
+      addNewTask(taskDescription);
+      newTaskInput.value = "";
+    }
+  });
+}
+
+function clearCompletedTasks() {
+  tasks = tasks.filter((task) => !task.completed);
+  renderTasks();
+}
+
+function addNewTask(taskDescription) {
+  const newTask = {
+    description: taskDescription,
+    completed: false,
+    icon: MyImage,
+  };
+  tasks.push(newTask);
+  renderTasks();
 }
 
 renderTasks();
